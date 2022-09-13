53f064f6e2236be28434510f0984f6165e65d9cd
= <=>
2022-09-13 15:33:40 -0300

    add: pagination in flights

--- resources/views/flightsComponent/flights.blade.php
+++ resources/views/flightsComponent/flights.blade.php
@@ -65,8 +65,9 @@
                 </tr>
                 </tbody>
             @endforeach
-            {{$flights->links()}}
+            
         </table>
+        {{$flights->links()}}
         <a href="/flights/add_flight">
             <button class="mt-6 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
                 Add Flight
@@ -82,6 +83,8 @@
             <p>{{session('success')}}</p>
         </div>
     @endif
+
+
 
     <script>
 
